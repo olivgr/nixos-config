63beda8f786dcd75e236445c0e3aa3ea7d8ada4d
Oliver Grimm <olivergrimm@fastmail.fm>
2026-06-07 11:19:21 +0900

    change default rate pipewire

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,7 @@
 	  extraConfig.pipewire."92-low-latency" = {
 		  context.properties = {
 			  default.clock.rate = 44100;
+			  default.clock.allowed-rates = [ 44100 ];
 			  default.clock.quantum = 64;
 			  default.clock.min-quantum = 64;
 			  default.clock.max-quantum = 512;
