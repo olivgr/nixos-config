b58247482871ee3042a9daa0b5d35e44f297bbc5
Oliver Grimm <olivergrimm@fastmail.fm>
2026-06-10 08:31:30 +0900

    activate wifi

--- configuration.nix
+++ configuration.nix
@@ -42,7 +42,7 @@
   };
 
   networking.hostName = "x230"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
   hardware.bluetooth.enable = false;
 
